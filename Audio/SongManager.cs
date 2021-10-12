9552b025fcb7f6da1690ddf61d01b9a3196bfe75
mysterypaint <mysterypainto@gmail.com>
2021-10-12 03:41:11 -0700

    Some playback... wrapper might be too slow for audio streaming?

--- Audio/SongManager.cs
+++ Audio/SongManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 using ChaiFoxes.FMODAudio;
 
 namespace OpenLaMulana
@@ -17,10 +19,47 @@ namespace OpenLaMulana
 
         public void LoadSong(string fileName)
         {
-
+            /*
             var sound = CoreSystem.LoadStreamedSound(fileName);
             var channel = sound.Play();
             channel.Looping = true;
+            */
+
+            //FMOD.CREATESOUNDEXINFO
+
+            /*
+            var sound = CoreSystem.Native.createSound("sdflk.mid", FMODMode.Core, FMOD.Sound.);
+            var channel = sound.Play();
+            channel.Looping = true;
+            */
+
+            /*
+            IntPtr pathInt = "gm.dls");
+            info.dlsname = pathInt;
+            info.cbsize = Marshal.SizeOf(info);
+            FMOD.Sound sound;
+            lowSys.createStream(Utils.dataPath + "errors.mid", FMOD.MODE.DEFAULT, ref info, out sound);
+            FMOD.Channel channel;
+            lowSys.playSound(sound, null, false, out channel);
+            Marshal.FreeHGlobal(pathInt);
+            */
+            var info = new FMOD.CREATESOUNDEXINFO();
+
+            string dataPath = Path.GetFullPath("Content/music/");
+
+            IntPtr pathInt = Marshal.StringToHGlobalAnsi(dataPath + "gm.dls");//SanbikiScc
+            info.dlsname = pathInt;
+            info.cbsize = Marshal.SizeOf(info);
+            
+            info.suggestedsoundtype = FMOD.SOUND_TYPE.IT;
+
+            CoreSystem.Native.createStream(dataPath + "m00.it", FMOD.MODE.LOOP_NORMAL, ref info, out FMOD.Sound sound);
+
+            FMOD.ChannelGroup channelGroup = new FMOD.ChannelGroup();
+            FMOD.Channel channel;
+            CoreSystem.Native.playSound(sound, channelGroup, false, out channel);
+            
+            Marshal.FreeHGlobal(pathInt);
         }
     }
 }
